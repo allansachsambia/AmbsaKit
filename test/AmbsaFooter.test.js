1d90474e9962dd19b78a20c2732dfbe4d04b28b5
Allan <allansachs@icloud.com>
2018-08-09 19:37:56 -0400

    Add tests for AmbsaFooter and testUtils

---
+++ test/AmbsaFooter.test.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr, checkProps } from "./testUtils";
+import AmbsaFooter from "../src/components/ambsa-footer/AmbsaFooter";
+
+/**
+ * Factory function to create a ShallowWrapper for the AmbsaFooter component
+ * @function setup
+ * @param {object} props - Component props specific to this setup.
+ * @param {any} state = Initial state for setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<AmbsaFooter {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
+};
+
+/**
+ * Rendering Test
+ */
+test("renders without errors", () => {
+  const wrapper = setup();
+  const ambsaFooterComponent = findByTestAttr(
+    wrapper,
+    "component-ambsa-footer"
+  );
+  expect(ambsaFooterComponent.length).toBe(1);
+});
+
+/**
+ * About Prop Tests
+ */
+describe("check 'about' prop values on ambsa-footer", () => {
+  test("does not throw a warning with expected props types", () => {
+    const expectedProps = { about: "" };
+    checkProps(AmbsaFooter, expectedProps);
+  });
+
+  test("it nests the about prop text inside the component with the data-test attribute value of component-ambsa-footer-about", () => {
+    const txt = "Lorem ipsum dolor sit amet.";
+    const wrapper = setup({ about: txt });
+    const el = findByTestAttr(wrapper, "component-ambsa-footer-about");
+    expect(el.text()).toBe(txt);
+  });
+});
+
+/**
+ * Copyright Prop Tests
+ */
+describe("check 'copyright' prop values on ambsa-footer", () => {
+  test("does not throw a warning with expected props types", () => {
+    const expectedProps = { copyright: "" };
+    checkProps(AmbsaFooter, expectedProps);
+  });
+
+  test("it nests the copyright prop text inside the component with the data-test attribute value of component-ambsa-footer-copyright", () => {
+    const txt = "Lorem ipsum dolor sit amet.";
+    const wrapper = setup({ copyright: txt });
+    const el = findByTestAttr(wrapper, "component-ambsa-footer-copyright");
+    expect(el.text()).toBe(txt);
+  });
+});
--- test/AmbsaHero.test.js
+++ test/AmbsaHero.test.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { shallow } from "enzyme";
+import { findByTestAttr, checkProps } from "./testUtils";
 import AmbsaHero from "../src/components/ambsa-hero/AmbsaHero";
 
 /**
- * Factory function to create a ShallowWrapper for the App component
+ * Factory function to create a ShallowWrapper for the AmbsaHero component
  * @function setup
  * @param {object} props - Component props specific to this setup.
  * @param {any} state = Initial state for setup.
@@ -13,16 +14,6 @@ const setup = (props = {}, state = null) => {
   const wrapper = shallow(<AmbsaHero {...props} />);
   if (state) wrapper.setState(state);
   return wrapper;
-};
-
-/**
- * Return ShallowWrapper containing node(s) with the given data-test value.
- * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within.
- * @param {string} val - Value of data-test attribute for search.
- * @returns {ShallowWrapper}
- */
-const findByTestAttr = (wrapper, val) => {
-  return wrapper.find(`[data-test="${val}"]`);
 };
 
 /**
@@ -38,20 +29,17 @@ test("renders without errors", () => {
  * Image Prop Tests
  */
 describe("check image prop values on ambsa-hero", () => {
+  test("does not throw a warning with expected props types", () => {
+    const expectedProps = { image: "" };
+    checkProps(AmbsaHero, expectedProps);
+  });
+
   test("it can set the backgroundImage inline style to the image prop value if a string with an image URL is passed to it", () => {
-    const imageVal = "https://picsum.photos/1800/1224/?image=465";
+    const imageVal = "./elizabeth-lies-190771-unsplash.jpg";
     const wrapper = setup({ image: imageVal });
     const ambsaHero = findByTestAttr(wrapper, "component-ambsa-hero");
     const containerStyle = ambsaHero.props().style;
     expect(containerStyle).toEqual({ backgroundImage: `url(${imageVal})` });
-  });
-
-  test("it will set the backgroundImage inline style to an empty object if the wrong data type is passed as an image prop", () => {
-    const imageVal = [];
-    const wrapper = setup({ image: imageVal });
-    const ambsaHero = findByTestAttr(wrapper, "component-ambsa-hero");
-    const containerStyle = ambsaHero.props().style;
-    expect(containerStyle).toEqual({});
   });
 
   test("it will set the backgroundImage inline style to an empty object if nothing is passed as an image prop", () => {
@@ -66,6 +54,11 @@ describe("check image prop values on ambsa-hero", () => {
  * Stick Prop Tests
  */
 describe("check stick prop values on ambsa-hero", () => {
+  test("does not throw a warning with expected props types", () => {
+    const expectedProps = { stick: "" };
+    checkProps(AmbsaHero, expectedProps);
+  });
+
   test("if `left` is passed, the data-stick attribute value should be set to `left`", () => {
     const propVal = "left";
     const wrapper = setup({ stick: propVal });
@@ -98,14 +91,6 @@ describe("check stick prop values on ambsa-hero", () => {
     expect(attrVal).toBe("");
   });
 
-  test("if a non-string type is passed, the data-stick attribute value should be set to an empty string", () => {
-    const propVal = [];
-    const wrapper = setup({ stick: propVal });
-    const el = findByTestAttr(wrapper, "component-ambsa-hero");
-    const attrVal = el.props()["data-stick"];
-    expect(attrVal).toBe("");
-  });
-
   test("if no props are passed, the data-stick attribute value should be set to an empty string", () => {
     const wrapper = setup();
     const el = findByTestAttr(wrapper, "component-ambsa-hero");
@@ -118,6 +103,11 @@ describe("check stick prop values on ambsa-hero", () => {
  * Brightness Prop Tests
  */
 describe("check brightness prop values on ambsa-hero-inner-wrap", () => {
+  test("does not throw a warning with expected props types", () => {
+    const expectedProps = { brightness: "" };
+    checkProps(AmbsaHero, expectedProps);
+  });
+
   test("if `darker` is passed, the data-brightness attribute value should be set to `darker`", () => {
     const propVal = "darker";
     const wrapper = setup({ brightness: propVal });
@@ -152,14 +142,6 @@ describe("check brightness prop values on ambsa-hero-inner-wrap", () => {
 
   test("if a string that is not `darker`, `dark`, `light`, or `lighter` is passed, the data-brightness attribute value should be set to an empty string", () => {
     const propVal = "x";
-    const wrapper = setup({ brightness: propVal });
-    const el = findByTestAttr(wrapper, "component-ambsa-hero-inner-wrap");
-    const attrVal = el.props()["data-brightness"];
-    expect(attrVal).toBe("");
-  });
-
-  test("if a non-string type is passed, the data-brightness attribute value should be set to an empty string", () => {
-    const propVal = [];
     const wrapper = setup({ brightness: propVal });
     const el = findByTestAttr(wrapper, "component-ambsa-hero-inner-wrap");
     const attrVal = el.props()["data-brightness"];
