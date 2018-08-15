37c2fecb31b1233ab943d28d094e7dd20c296836
Allan <allansachs@icloud.com>
2018-08-15 12:32:48 -0400

    Update version and make AmbsaFooter exportable

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AmbsaHero from "./components/ambsa-hero/AmbsaHero";
+import AmbsaFooter from "./components/ambsa-footer/AmbsaFooter";
 
 export default class Index extends React.Component {
   render() {
     return null;
   }
 }
-export { AmbsaHero };
+export { AmbsaHero, AmbsaFooter };
